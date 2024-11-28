5d4cd22095efb19f87f3d197202d2d8e83dfd62d
Matteolm23 <matteo.lamalfa@davincimilazzo.edu.it>
2024-11-28 18:30:38 +0100

    Add files via upload

---
+++ es_cerchio.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"es_cerchio_math.h"
+#define CIRCUMFERENCE(r) 2*r*PI
+#define AREA(r) PI*(r*r)
+
+int main(void){
+
+    float r, a = AREA(r), c = CIRCUMFERENCE(r);
+
+    printf("inserisci il raggio del cerchio: ");
+    scanf("%f", &r);
+    printf("la circonferenza del cerchio e' %f\n", c);
+    printf("l'area del cerchio e' %f\n", a);
+
+    return 0;
+
+}
